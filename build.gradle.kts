29a116a3b2580270317a341b237d0a7bee145afe
abdullatheeftk-gulf <68467790+abdullatheeftk-gulf@users.noreply.github.com>
2024-02-18 07:09:28 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("io.ktor.plugin") version "2.3.8"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
+    id("org.gretty") version "4.0.3"
+    id("war")
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.tomcat.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+gretty {
+    servletContainer = "tomcat10"
+    contextPath = "/"
+    logbackConfigFile = "src/main/resources/logback.xml"
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-tomcat-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("io.ktor:ktor-server-servlet-jakarta:$ktor_version")
+
+}
+
+afterEvaluate {
+    tasks.getByName("run") {
+        dependsOn("appRun")
+    }
+}
